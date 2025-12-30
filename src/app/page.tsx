4819ab8db3487d1d3b1dcb23d66663d777151bff
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2025-12-30 19:45:49 +0900

    feat : Removed Header from Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="bg-white w-[100vw] h-[100vh] text-black flex flex-col pl-[20%] pr-[20%]">
+    <div className="bg-white h-[90%] text-black flex flex-col">
       <div className="border-l-1 border-r-1  border-black bg-grey-100 h-full w-full">
-        <div className="flex justify-start items-start p-5 h-[10%]">
-          {/* Header */}
-          <div className="w-[80%] flex justify-center items-center">
-            <h1>ELGRATIO F C</h1>
-          </div>
-          <nav className="flex w-full justify-end gap-5 w-[10%]">
-            <div className="border-1 p-3">
-              <Link href="/">Home</Link>
-            </div>
-            <div className="border-1 p-3">
-              <Link href="/blog">Blog</Link>
-            </div>
-          </nav>
-        </div>
-
-        <div className=" grid grid-cols-2 grid-rows-4 gap-4 h-[90%] w-full p-2">
+        <div className=" grid grid-cols-2 grid-rows-4 gap-4 h-full w-full p-2">
           {/* CONTENT */}
           <div className="border-1">
             <div className="flex justify-center">
@@ -38,8 +23,20 @@ export default function Home() {
             </div>
           </div>
           <div className=" col-start-2 row-start-4">
-            <div className="flex justify-center">
-              <h1>Contact</h1>
+            <div>
+              <div className="flex justify-center">
+                <h1>Contact</h1>
+              </div>
+              <div className="w-full">
+                <Link href="https://linkedin.com/in/elgratiofc/">
+                  <Image
+                    src="/linkedin-dark.svg"
+                    alt="Linkedin Logo"
+                    width={50}
+                    height={50}
+                  ></Image>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
