7eb9edb66650a569e0730d1060ff8a2967a3020e
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-01-02 15:36:15 +0900

    feat : Implement Project Card and UI Clean Ups

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import ProjectCard from "./components/project-card";
 
 export default function Home() {
   return (
@@ -7,18 +8,51 @@ export default function Home() {
       <div className="border-l-1 border-r-1  border-black bg-grey-100 h-full w-full">
         <div className=" grid grid-cols-2 grid-rows-4 gap-4 h-full w-full p-2">
           {/* CONTENT */}
-          <div className="border-1">
-            <div className="flex justify-center">
+          <div className="border-1 flex p-1">
+            {/* <div className="flex justify-start align-center p-3 border-b-1">
               <h1>Summary</h1>
-            </div>
+            </div> */}
+            An aspiring Full Stack Developer, with 3+ years Experience in Game Development Industry.
           </div>
           <div className="border-1 row-span-3 col-start-1 row-start-2">
-            <div className="flex justify-center">
+            <div className="flex justify-start align-center p-3 border-b-1">
               <h1>Projects</h1>
+            </div>
+            <div className="flex flex-col p-2 overflow-y-auto max-h-full gap-1">
+              <ProjectCard
+                name="Sentence Repeater"
+                description="A tool to help you duplicate a sentence with Variable Definition features. That allows you to duplicate a sentence with specific rules."
+                logoUrl="/sentence-rep-logo.png"
+              />{" "}
+              <ProjectCard
+                name="Piper's Pet Cafe"
+                description="Piper’s Pet Cafe is a casual mobile game that blends card-based solitaire puzzles with a unique cafe renovation theme."
+                logoUrl="/ppc-logo.png"
+              />
+              <ProjectCard
+                name="Test"
+                description="Test123"
+                logoUrl="/linkedin-dark.svg"
+              />
+              <ProjectCard
+                name="Test"
+                description="Test123"
+                logoUrl="/linkedin-dark.svg"
+              />
+              <ProjectCard
+                name="Test"
+                description="Test123"
+                logoUrl="/linkedin-dark.svg"
+              />
+              <ProjectCard
+                name="Test"
+                description="Test123"
+                logoUrl="/linkedin-dark.svg"
+              />
             </div>
           </div>
           <div className="border-1 row-span-3 col-start-2 row-start-1">
-            <div className="flex justify-center">
+           <div className="flex justify-start align-center p-3 border-b-1">
               <h1>Work History</h1>
             </div>
           </div>
@@ -34,7 +68,7 @@ export default function Home() {
                     alt="Linkedin Logo"
                     width={50}
                     height={50}
-                  ></Image>
+                  />
                 </Link>
               </div>
             </div>
