39463faec0284d02fab3f2e882c746dac54a5570
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-01-03 14:05:44 +0900

    feat : Simplified Work Card

--- src/app/components/work-card.tsx
+++ src/app/components/work-card.tsx
@@ -1,31 +1,49 @@
+import Link from "next/link";
+
 interface WorkHistoryCardProps {
   role: string;
   duration: string;
   company: string;
+  companyLink?: string;
   details?: string;
 }
 
-const WorkHistoryCard: React.FC<WorkHistoryCardProps> = ({role,duration,company,details}) => {
+const WorkHistoryCard: React.FC<WorkHistoryCardProps> = ({
+  role,
+  duration,
+  company,
+  companyLink,
+  details,
+}) => {
   return (
     <>
-      <div className="flex w-full">
-        <div className="border-1 p-2 w-[90%]">
+      <div className="border-1 hover:border-black border-white flex w-full">
+        <div className="p-2 w-full flex flex-col gap-2">
           <div>
-            <p className="text-2xl font-bold">{role}</p>
+            <div>
+              <p className="text-lg font-bold">{role}</p>
+            </div>
+            <div>
+              <p className="font-bold">{duration}</p>
+            </div>
           </div>
+
           <div>
-            <p className="font-bold">{duration}</p>
+            {companyLink ? (
+              <Link href={`https://${companyLink}`}>{company}</Link>
+            ) : (
+              <p className="italic">{company}</p>
+            )}
           </div>
-          <div>
-            <p className="italic">{company}</p>
-          </div>
-          <div>
-            <p>{details}</p>
-          </div>
+          {details ? (
+            <div>
+              <p>{details}</p>
+            </div>
+          ) : null}
         </div>
-        <div className="w-[10%] flex justify-center items-center">
+        {/* <div className="w-[10%] flex justify-center items-center">
           <div className="bg-black rounded-full border-1 w-[10px] h-[10px]"></div>
-        </div>
+        </div> */}
       </div>
     </>
   );
