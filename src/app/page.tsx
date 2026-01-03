5b9a8b12e9b87587f601e9ebf423b588d7b38a1f
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-01-03 14:04:36 +0900

    feat : Adjusted Tagline and Professional Experience Data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,53 +9,77 @@ export default function Home() {
       <div className="border-l-1 border-r-1  border-black bg-grey-100 h-full w-full">
         <div className=" grid grid-cols-2 grid-rows-4 gap-4 h-full w-full p-2">
           {/* CONTENT */}
-          <div className="border-1 flex p-1">
+          <div className="border-1 w-full h-full">
             {/* <div className="flex justify-start align-center p-3 border-b-1">
               <h1>Summary</h1>
             </div> */}
-            An aspiring Full Stack Developer, with 3+ years Experience in Game
-            Development Industry.
-          </div>
-          <div className="border-1 row-span-3 col-start-1 row-start-2 overflow-hidden">
-            <div className="flex justify-start align-center p-3 border-b-1 ">
-              <h1 className="text-2xl font-bold">Experience</h1>
+            <div className="flex  flex-col p-1 justify-end align-end p-5">
+              <p className="text-start text-2xl font-bold">
+                An aspiring Full Stack Developer
+              </p>
+              <p className="italic text-xl">with 3+ years Experience in Game Development Industry</p>
             </div>
-            <div className="flex flex-col p-2 overflow-y-auto max-h-full gap-1">
-              {/* <WorkHistoryCard
-                role="Cutscene Designer Intern and Apprentice"
-                duration="Jan 2022 - Oct 2022"
+          </div>
+          <div className="border-1 row-span-3 col-start-1 row-start-2 overflow-hidden flex flex-col">
+            <div className="flex justify-start align-center p-3 border-b-1 flex-shrink-0">
+              <h1 className="text-2xl font-bold">Professional Experience</h1>
+            </div>
+            <div className="flex flex-col p-2 overflow-y-auto flex-1 gap-1">
+              <WorkHistoryCard
+                role="Project Coordinator"
+                duration="Jul 2025 - Aug 2025"
+                company="Script Smelter Studios"
+                details="Designed in-game Isometric Cutscene using in-house tools."
+              />
+              <WorkHistoryCard
+                role="Mid Cutscene Designer"
+                duration="Mar 2023 - Feb 2025"
+                company="Tripledot Studios"
+                companyLink="www.linkedin.com/company/tripledot-studios"
+                details="Designed in-game Isometric Cutscene using in-house tools."
+              />
+              <WorkHistoryCard
+                role="Junior Cutscene Designer"
+                duration="Oct 2022 - Mar 2023"
                 company="Touchten Games"
+                companyLink="www.linkedin.com/company/touchtengames"
                 details="Designed in-game Isometric Cutscene using in-house tools."
               />
               <WorkHistoryCard
                 role="Cutscene Designer Intern and Apprentice"
                 duration="Jan 2022 - Oct 2022"
                 company="Touchten Games"
+                companyLink="www.linkedin.com/company/touchtengames"
                 details="Designed in-game Isometric Cutscene using in-house tools."
-              /> */}
+              />
             </div>
           </div>
-          <div className="border-1 row-span-3 col-start-2 row-start-1 overflow-hidden">
-            <div className="flex justify-start align-center p-3 border-b-1">
+          <div className="border-1 row-span-3 col-start-2 row-start-1 overflow-hidden flex flex-col">
+            <div className="flex justify-start align-center p-3 border-b-1 flex-shrink-0">
               <h1 className="text-2xl font-bold">Projects</h1>
             </div>
-            <div className="flex flex-col p-2 overflow-y-auto h-full gap-2">
+            <div className="flex flex-col p-2 overflow-y-auto flex-1 gap-2 ">
               <ProjectCard
                 name="Personal Website"
                 description="A Blogpost-style Portofolio Website that conveys how El developed his Projects"
                 logoUrl="/efc-logo.png"
-                techStacks={["/next.svg","/tailwind-logo.svg"]}
+                techStacks={["/next.svg", "/tailwind-logo.svg"]}
               />
               <ProjectCard
                 name="Sentence Repeater"
                 description="A tool to help you duplicate a sentence with Variable Definition features. That allows you to duplicate a sentence with specific rules."
                 logoUrl="/sentence-rep-logo.png"
-                techStacks={["/react-logo.svg","/expressjs-logo.svg","/tailwind-logo.svg"]}
+                techStacks={[
+                  "/react-logo.svg",
+                  "/expressjs-logo.svg",
+                  "/tailwind-logo.svg",
+                ]}
               />
               <ProjectCard
                 name="Piper's Pet Cafe"
                 description="A casual mobile game that blends card-based solitaire puzzles with a unique cafe renovation theme."
                 logoUrl="/ppc-logo.png"
+                techStacks={["/unity-icon.svg"]}
               />
               {/* <ProjectCard
                 name="Test"
@@ -81,7 +105,7 @@ export default function Home() {
           </div>
           <div className=" col-start-2 row-start-4">
             <div>
-              <div className="flex justify-start items-center">
+              <div className="flex justify-start items-center p-2">
                 <h1 className="text-xl font-bold">Contact</h1>
               </div>
               <div className="w-full p-3 flex gap-10">
