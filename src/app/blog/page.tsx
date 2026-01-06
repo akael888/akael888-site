7655a4eb0ab4ab6d56ff220c83b263d1a977e2fc
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-01-06 12:03:14 +0900

    feat : Adjusted Blog Page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,13 +1,23 @@
 import Link from "next/link";
+import BlogpostCard from "../components/blogpost-card";
 
 export default function blog({}) {
   return (
-    <div className="bg-white h-[90%] text-black flex flex-col">
+    <div className="bg-white h-[100vh] text-black flex flex-col">
       <div className="border-l-1 border-r-1  border-black bg-grey-100 h-full w-full">
-        <div className="min-h-[90%] grid grid-cols-3 grid-rows-4 gap-4 p-4">
-          <div className="border-1 col-span-2 row-span-4">Blog Post</div>
-          <div className="border-1 row-span-3 col-start-3">Projects</div>
-          <div className="border-1 col-start-3 row-start-4">Contact</div>
+        <div className="flex flex-col gap-4 p-4">
+          <div className="border-1 ">
+            <div className="flex justify-start align-center p-3 border-b-1 flex-shrink-0">
+              <h1 className="text-2xl font-bold">Recent Posts..</h1>
+            </div>
+            <div className="p-1">
+              <BlogpostCard
+                title="Building a Variable-Based String Iteration Tool"
+                description="A CRA Post Mortem"
+                coverImagePath="/blog-images/PJ01/PJ01-screenshot.png"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
