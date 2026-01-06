799bdb02571db5335f38ecfaf19d0ed0eeac3285
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-01-06 13:22:23 +0900

    chore : Temp BlogSpot Page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,13 +2,14 @@ import Link from "next/link";
 
 export default function blog({}) {
   return (
-    <div className="bg-white h-[90%] text-black flex flex-col">
+    <div className="bg-white h-screen text-black flex flex-col">
       <div className="border-l-1 border-r-1  border-black bg-grey-100 h-full w-full">
-        <div className="min-h-[90%] grid grid-cols-3 grid-rows-4 gap-4 p-4">
+        {/* <div className="min-h-[90%] grid grid-cols-3 grid-rows-4 gap-4 p-4">
           <div className="border-1 col-span-2 row-span-4">Blog Post</div>
           <div className="border-1 row-span-3 col-start-3">Projects</div>
           <div className="border-1 col-start-3 row-start-4">Contact</div>
-        </div>
+        </div> */}
+        <div className="p-1"> In Progress..</div>
       </div>
     </div>
   );
