224af22905a85d0184ac6cc5c2c722c322433b8d
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-01-07 19:34:12 +0900

    feat : Added Pinned Features, and Adjusted the BlogPost Card

--- src/app/components/blogpost-card.tsx
+++ src/app/components/blogpost-card.tsx
@@ -3,37 +3,54 @@ import Image from "next/image";
 interface BlogpostCardProps {
   title: string;
   description: string;
-  coverImagePath: string;
+  coverImagePath?: string;
+  pinnedImagePath?: string;
   imageContainerClassName?: string;
+  pinned?: boolean;
 }
 
 const BlogpostCard: React.FC<BlogpostCardProps> = ({
   title,
   description,
-  coverImagePath,
-  imageContainerClassName = "w-full aspect-video",
+  coverImagePath = "/blog-img/default/cover-default.png",
+  pinnedImagePath = "/blog-img/default/pinned-default.png",
+  pinned = false,
+  imageContainerClassName,
 }) => {
   return (
     <>
-      <div className=" p-2 grid grid-cols-2 grid-rows-1">
-        <div className="flex flex-col">
-          <div className="h-full w-full flex flex-col items-start justify-center">
-            <h1 className="text-2xl font-bold">{title}</h1>
-            <h1>{description}</h1>
-            <div className="flex flex-row">
-              <button>Read more..</button>
+      <div
+        className={` w-full h-full p-2 flex flex-col ${
+          pinned ? "col-span-full" : null
+        }`}
+      >
+        <div className="h-full grid grid-cols-2 grid-rows-2 ">
+          <div className="w-full h-full flex justify-center items-center row-span-2 p-10">
+            <div
+              className={`relative mt-2 ${
+                imageContainerClassName
+                  ? imageContainerClassName
+                  : pinned
+                  ? "w-full aspect-video"
+                  : "w-full aspect-square"
+              }`}
+            >
+              <Image
+                src={pinned ? pinnedImagePath : coverImagePath}
+                alt={`${title} Cover Image`}
+                fill
+                className="object-cover border-2 border-black"
+                sizes="(max-width: 768px) 100vw, 50vw"
+              />
             </div>
           </div>
-        </div>
-
-        <div className={`relative mt-2 ${imageContainerClassName}`}>
-          <Image
-            src={coverImagePath}
-            alt={`${title} Cover Image`}
-            fill
-            className="object-cover border-2 border-black"
-            sizes="(max-width: 768px) 100vw, 300px"
-          />
+          <div className="h-full w-full flex flex-col items-right justify-center row-span-2 pl-10 pr-10 gap-2">
+            <h1 className="text-2xl font-bold">{title}</h1>
+            <h1>{description}</h1>
+            <div className="flex flex-row w-full col-span-2  justify-start">
+              <button className="border-1 p-1">Read more..</button>
+            </div>
+          </div>
         </div>
       </div>
     </>
