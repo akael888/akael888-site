9a2ca5e66fd6eab06bc473aba249060bfb6b18b2
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-01-11 23:35:44 +0900

    feat : Separate the Font Improrting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Roboto_Condensed} from "next/font/google";
+import { roboto_condensed } from "./font";
 import "./globals.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-const roboto_condensed = Roboto_Condensed({
-  subsets: ['latin'],
-  display: 'swap', // 'swap' ensures text is visible during font load
-  variable: '--font-roboto-condensed', // Define a CSS variable name
-  weight: ['400', '700'], // Specify the weights you want to use
-});
 
 export const metadata: Metadata = {
   title: "Elgratio FC",
