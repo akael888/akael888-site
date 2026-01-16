df3ec5a57ae7d65e9416fe75e010580b4a796ccc
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-01-16 22:08:48 +0900

    feat : Added Blog URL for Project Card

--- src/app/ui/home/project-card.tsx
+++ src/app/ui/home/project-card.tsx
@@ -9,6 +9,7 @@ interface ProjectCardProps {
   techStacks?: string[];
   techStacksHoverText?: string[];
   appUrl?: string;
+  blogUrl?: string;
 }
 
 const ProjectCard: React.FC<ProjectCardProps> = ({
@@ -18,11 +19,12 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
   techStacks = ["/next.svg"],
   techStacksHoverText = ["Hover Text"],
   appUrl,
+  blogUrl,
 }) => {
   return (
     <>
       <div>
-        <div className="border-1 border-white hover:border-black">
+        <div className="">
           <div className="grid grid-row-2 grid-cols-3 p-2">
             <div className=" row-span-2 flex justify-center items-center p-2 ">
               <div className="w-fit border-1 p-2">
@@ -43,9 +45,9 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
             </div>
           </div>
           <div className="w-full max-h-[100px] p-3 grid grid-row-1 grid-cols-2 justify-center">
-            <div className="flex align-center p-2 w-[60%] gap-1">
+            <div className="flex justify-left align-center p-2 w-[60%] gap-1">
               {techStacks.map((stack, index) => (
-                <div key={index} className="rounded-10 p-1 flex justify-center">
+                <div key={index} className="rounded-10 p-1 flex justify-center align-center">
                   <LogoIMGTemplate
                     imgPath={stack}
                     imgAlternative="Stack Logo"
@@ -59,15 +61,18 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
             <div className="flex flex-row justify-end items-center gap-2">
               {appUrl ? (
                 <Link
-                  className="border-1 w-[20%] p-1 hover:bg-gray-600 justify-center items-center flex"
+                  className="border-1 w-[20%] p-1 hover:border-black border-white justify-center items-center flex"
                   href={`https://${appUrl}`}
                 >
                   App
                 </Link>
               ) : null}
-              {/* <button className="border-1 w-[20%] p-1 hover:bg-gray-600 justify-center items-center flex">
+              <Link
+                className="border-1 w-[20%] p-1 hover:border-black border-white justify-center items-center flex"
+                href={`${blogUrl}`}
+              >
                 Blog
-              </button> */}
+              </Link>
             </div>
           </div>
         </div>
