1f50ab810308cd41b7631bdf457d1f4339d06674
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-01-18 01:13:37 +0900

    feat : Added Link in Logo Name

--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -9,14 +9,16 @@ export default function Header() {
       <header className="flex justify-start items-center p-5 h-[10%] bg-bg border-border text-text border-l-1 border-r-1 border-b-1 sticky top-0 z-100">
         {/* Header */}
         <div className="w-full flex justify-start items-center h-full">
-          <strong>
-            <h1 className="md:text-2xl text-xl font-bold">ELGRATIO F C</h1>
-          </strong>
+          <Link href="/">
+            <strong>
+              <h1 className="md:text-2xl text-xl font-bold">ELGRATIO F C</h1>
+            </strong>
+          </Link>
         </div>
 
         <nav className="flex w-full items-center justify-end gap-2 w-[10%]">
           <DarkModeButton />
-          <NavDropDown/>
+          <NavDropDown />
         </nav>
       </header>
     </>
