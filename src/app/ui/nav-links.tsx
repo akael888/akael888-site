e6bf3916222cf46c215c43a6c6695b7c37f99044
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-01-17 17:03:35 +0900

    feat : UI Improvements on Header and Nav Links

--- src/app/ui/nav-links.tsx
+++ src/app/ui/nav-links.tsx
@@ -7,8 +7,8 @@ import { usePathname } from "next/navigation";
 export default function NavLinks() {
   const pathname = usePathname();
   const links = [
-    { name: "Home", href: "/" },
-    { name: "Blog", href: "/blog" },
+    { name: "home", href: "/" },
+    { name: "blog", href: "/blog" },
   ];
 
   return (
@@ -16,13 +16,13 @@ export default function NavLinks() {
       {links.map((link) => {
         const isActive = link.href == pathname;
         return (
-          <div className="flex items-center" key={link.name}>
+          <div className="flex w-fit items-center" key={link.name}>
             <Link
               href={link.href}
-              className={clsx("border-1 border-white p-3", {
-                "bg-black text-white pointer-events-none cursor-not-allowed":
+              className={clsx("border-1  md:text-base text-sm border-bg p-3", {
+                "bg-text text-bg pointer-events-none cursor-not-allowed":
                   isActive,
-                "hover:border-black": !isActive,
+                "bg-bg text-text hover:border-border": !isActive,
               })}
             >
               {link.name}
