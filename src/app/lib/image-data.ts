ebdbe4aa8fd3a7e7c60e268d467b25d7f6f9eb5c
Elgratio Latuihamallo <39221603+akael888@users.noreply.github.com>
2026-01-17 13:37:48 +0900

    feat : Setup Logo Data

---
+++ src/app/lib/image-data.ts
@@ -0,0 +1,54 @@
+const logoData = {
+  javascript: {
+    imagePath: "/javascript-logo.svg",
+    imageAlternative: "Javascript Logo",
+    imageHoverText: "Javascript",
+  },
+  python: {
+    imagePath: "/python-logo.svg",
+    imageAlternative: "Python Logo",
+    imageHoverText: "Python",
+  },
+  reactjs: {
+    imagePath: "/react-logo.svg",
+    imageAlternative: "React JS Logo",
+    imageHoverText: "React JS",
+  },
+  nextjs: {
+    imagePath: "/next.svg",
+    imageAlternative: "Next JS Logo",
+    imageHoverText: "Next JS",
+  },
+  nodejs: {
+    imagePath: "/nodejs-logo.svg",
+    imageAlternative: "Node JS Logo",
+    imageHoverText: "Node JS",
+  },
+  expressjs: {
+    imagePath: "/expressjs-logo.svg",
+    imageAlternative: "Express JS Logo",
+    imageHoverText: "Express JS",
+  },
+  vercel: {
+    imagePath: "/vercel-logo.svg",
+    imageAlternative: "Vercel Logo",
+    imageHoverText: "Vercel",
+  },
+  mongodb: {
+    imagePath: "/mongodb-logo.svg",
+    imageAlternative: "MongoDB Logo",
+    imageHoverText: "MongoDB",
+  },
+  unity: {
+    imagePath: "/unity-icon.svg",
+    imageAlternative: "Unity Logo",
+    imageHoverText: "Unity",
+  },
+  tailwindcss: {
+    imagePath: "/tailwind-logo.svg",
+    imageAlternative: "Tailwind Logo",
+    imageHoverText: "Tailwind",
+  },
+};
+
+export { logoData };
